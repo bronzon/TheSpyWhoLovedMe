a53c447696133045ac3bf048fce8af6e927c71b9
bronzon <olle.back@gmail.com>
2017-03-02 21:31:59 +0100

    sprite animation

--- Assets/startmovieclip.cs
+++ Assets/startmovieclip.cs
@@ -6,7 +6,7 @@ public class startmovieclip : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+		//((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
 	}
 	
 	// Update is called once per frame
